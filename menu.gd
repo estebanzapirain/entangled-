4c5e4d1c72374173596dc2725080569d9b457045
estebanzapirain <estebanzapirain@gmail.com>
2020-04-23 09:51:14 -0300

    animacion de menu de Bob

--- menu.gd
+++ menu.gd
@@ -9,7 +9,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$aliceAnimPlayer.play("aliceWalk")
-
+	$bobAnimPlayer.play("bobWalk")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
