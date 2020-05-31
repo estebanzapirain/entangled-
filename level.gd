f5f9496db89d8e16a3e893ea0534ffdb05d181e3
estebanzapirain <estebanzapirain@gmail.com>
2020-05-31 18:57:07 -0300

    Restart Level

--- level.gd
+++ level.gd
@@ -8,18 +8,35 @@ const BobStartPos = Vector2(236,75)
 
 onready var alice = get_parent().get_node("Alice")
 onready var bob = get_parent().get_node("Bob")
+onready var observer = get_node("Observer/KinematicBody2D")
+onready var timer = get_parent().get_node("Timer")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	alice.position = AliceStartPos
 	bob.position = BobStartPos
-	pass # Replace with function body.
-
+	observer.connect("was_observed",self,"player_observed")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func player_observed(character):
+#	print(character,"was observed")
+	if timer.is_stopped():
+		timer.start()
+		print(character,"was observed")
+
+
+func _on_Timer_timeout():
+	get_tree().reload_current_scene()
+#	alice.position = AliceStartPos
+#	bob.position = BobStartPos
 
 
 func _on_Exit_body_entered(body):
 	print("Salida");
+
+
+
+
+
--- observer.gd
+++ observer.gd
@@ -5,12 +5,14 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 var frames_array = [0,8,4,12]; # número de frame para la rotación, en la textura del Sprite observer
-var ray_array = [[1000,0],[0, -1000],[-1000,0],[0, 1000]]; #dirección (x,y) del RayCast acompañando la rotación
-var iFrame = 1;
+var ray_array = [[0,1000],[1000,0],[0, -1000],[-1000,0]]; #dirección (x,y) del RayCast acompañando la rotación
+var iFrame = 0;
 var wait_time = 2;
 onready var timer = get_node("Timer");
 onready var sprite = get_node("Sprite");
 onready var raycast = get_node("RayCast2D");
+
+signal was_observed(character)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,5 +24,10 @@ func _on_Timer_timeout():
 	sprite.frame = frames_array[iFrame % 4];
 	raycast.cast_to.x = ray_array[iFrame % 4][0];
 	raycast.cast_to.y = ray_array[iFrame % 4][1];
-	if (raycast.get_collider().get_owner().name == "Alice" || raycast.get_collider().get_owner().name == "Bob"):
-		print("Murio");
+
+
+func _process(delta):
+	if (raycast.get_collider().get_owner().name == "Alice"):
+		emit_signal("was_observed","Alice")
+	if (raycast.get_collider().get_owner().name == "Bob"):
+		emit_signal("was_observed","Bob")
